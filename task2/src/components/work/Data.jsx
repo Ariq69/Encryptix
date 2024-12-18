e6e26c36e7baab06b6454e882a3583198ea627d8
Ariq69 <106203189+Ariq69@users.noreply.github.com>
2024-12-18 11:36:04 +0700

    task 2 complete and edit readme file for access to task 2

---
+++ task2/src/components/work/Data.jsx
@@ -0,0 +1,60 @@
+import Work1 from "../../assets/work1.png"
+import Work2 from "../../assets/work2.png"
+// import Work3 from "../../assets/work3.jpg"
+// import Work4 from "../../assets/work4.jpg"
+// import Work5 from "../../assets/work5.jpg"
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "Laporan Kunjungan Pasien RS Haji Jakarta",
+      category: "real project",
+      link: "https://github.com/Ariq69/laporan-kunjungan-pasien-rs-haji-jakarta",
+      description: "Jakarta Hajj Hospital patient visit report website project using laravel framework",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "Sumare ERP Software Landing Page",
+      category: "dummy project",
+      link: "https://ariq69.github.io/Encryptix/task1",
+      description: "Developed the landing page for the Sumare ERP Software project using HTML, CSS, and JavaScript.",
+    },
+    // {
+    //   id: 3,
+    //   image: Work3,
+    //   title: "Brand design",
+    //   category: "dummy project",
+    //   link: "https://boxicons.com/?query=arro",
+    //   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto maiores quibusdam, nobis autem aspernatur sunt reiciendis eligendi doloribus consectetur vel sequi enim praesentium est, consequuntur amet, tempore distinctio dolores deserunt.",
+    // },
+    // {
+    //   id: 4,
+    //   image: Work4,
+    //   title: "App movil",
+    //   category: "dummy project",
+    //   link: "https://www-termsfeed-com.translate.goog/blog/sample-copyright-notices/?_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=tc",
+    //   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto maiores quibusdam, nobis autem aspernatur sunt reiciendis eligendi doloribus consectetur vel sequi enim praesentium est, consequuntur amet, tempore distinctio dolores deserunt.",
+    // },
+    // {
+    //   id: 5,
+    //   image: Work5,
+    //   title: "Web design",
+    //   category: "dummy project",
+    //   link: "https://www.youtube.com/watch?v=pYA_006zROs",
+    //   description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto maiores quibusdam, nobis autem aspernatur sunt reiciendis eligendi doloribus consectetur vel sequi enim praesentium est, consequuntur amet, tempore distinctio dolores deserunt.",
+    // },
+  ];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'real project',
+    },
+    {
+        name: 'dummy project',
+    },
+];
